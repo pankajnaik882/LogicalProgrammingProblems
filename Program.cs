335458a3f38ab095eb2e6e69a8a1dfafccc161c1
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-15 01:16:17 +0530

    Add : Solved Simulating StopWatch Program

--- Program.cs
+++ Program.cs
@@ -1,36 +1,29 @@
-﻿namespace Program
+﻿using System.Diagnostics;
+
+namespace LogicalProgram
 {
-    public class logical
+    public class Program
     {
         public static void Main(string[] args)
         {
-            int first=0;
-            int temp;
-            Console.WriteLine("how many coupon number you required ?");
-            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press Enter the Stop Watch Time : ");
+            int second = Convert.ToInt32(Console.ReadLine());
+           
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Stop Watch is Started ");
+            stopwatch.Start();
 
-            Console.WriteLine("genreted coupon number");
-
-            for (int i = 0; i < Number; i++)
+            // ... This takes 10 seconds to finish.
+            for (int i = 0; i < second; i++)
             {
-                Random number = new Random();
-                int coupon = number.Next(10000, 100000);
-
-                Console.WriteLine("Coupon : "+coupon);
-                Console.ReadLine();
-                temp = coupon;
-                first = temp;
-
-                if (first != temp)
-                {
-                    Console.WriteLine("Coupon Are same");
-                }
-                else
-                {
-                    Console.WriteLine("Coupon are Different");
-                }
-                
+                System.Threading.Thread.Sleep(100);
             }
+
+            // Stop.
+            stopwatch.Stop();
+
+            // Write hours, minutes and seconds.
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", stopwatch.Elapsed); 
         }
     }
 }
