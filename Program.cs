120cb73003af25d3a29d2e7b940ccbc3adf67330
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-15 01:05:50 +0530

    Add : Solved Fibonacci Series Program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace LogicalProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A value for FabSeries :-");
+            int fabS = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(fabS);
+            int num1 = 0;
+            int num2 = 1;
+            int nextNum;
+            if (fabS < 2)
+            {
+                Console.WriteLine("Enter a value greater than 2 if u want to see Fabseries :");
+            }
+            else
+            {
+                Console.Write(num1 + " " + num2 + " ");
+                for (int i = 2; i < fabS; i++)
+                {
+                    nextNum = num1 + num2;
+                    Console.Write(nextNum + " ");
+                    num1 = num2;
+                    num2 = nextNum;
+                }
+            }    
+        }
+    }
+}
